6a0f2ce91f771acea22efe27116ee50c505dd9e3
ediguerrero <edixon_guerrero96@hotmail.com>
2022-08-17 21:48:47 -0500

    add change file

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = FastAPI()
 
 @app.get("/roleUsers/{encryptedToken}")
 def read_item(encryptedToken: str):
-    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/roleUsers?encryptedToken=' + encryptedToken
+    url = 'https://62fc4666abd610251c17fdae.mockapi.io/api/roleUsers?encryptedToken='+encryptedToken
     response = requests.get(url, {}, timeout=5)
     return {"items": response.json()}
